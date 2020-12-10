53abd7245915be1b95f423d6eb29e4ec319ba97d
djungermann <david.jungermann@gmail.com>
2020-12-10 10:13:02 +0100

    Git restruct

---
+++ Arduino_examples/ikaros_modules/Arduino/Arduino.h
@@ -0,0 +1,26 @@
+//
+//  Arduino.h
+//
+//
+//  Created by David Jungermann on 2020-12-03.
+//
+
+#ifndef Arduino_
+#define Arduino_
+
+#include "IKAROS.h"
+#include "stdio.h"
+class Arduino: public Module
+{
+public:
+    Serial *s;
+    char    *rcvmsg;
+    Arduino(Parameter * p);
+    virtual ~Arduino();
+    static Module *Create(Parameter * p);
+    void Init();
+    void PrintValue();
+    void Tick();
+    float *	output;
+};
+#endif /* Arduino_h*/
