9fe76cef1b9346702e30ed1c39774e3f4dcdfdf8
djungermann <david.jungermann@gmail.com>
2020-12-15 13:29:10 +0100

    removed unused code

--- Arduino.cc
+++ Arduino.cc
@@ -22,8 +22,8 @@ Module *Arduino::Create(Parameter *p)
 
 Arduino::Arduino(Parameter *p) : Module(p)
 {
-
     s = NULL;
+
     /* Retrieves port where Arduino is connected from class file. Default is /dev/ttyACM0 on my Ubuntu 20.10 machine. */
     s = new Serial(GetValue("port"), 9600);
 }
@@ -45,24 +45,6 @@ void Arduino::Tick()
     int count = s->ReceiveBytes(rcvmsg, 100);
     std::cout << rcvmsg;
     std::cout << "\n";
-    std::stringstream stream(rcvmsg);
-    int i = 0;
-    while (1)
-    {
-        int n;
-        stream >> n;
-        if (!stream)
-            break;
-        int val = n;
-        output[i] = val;
-        std::cout << val << " ";
-        i++;
-    }
-}
-
-void Arduino::PrintValue()
-{
-    std::cout << "Value printed" << std::endl;
 }
 
 static InitClass init("Arduino", &Arduino::Create, "Source/UserModules/Arduino/");
